31eb0eec47d08b8b9a1862f8089205e736fbd6d5
Kidney <1796644973@qq.com>
2023-04-18 21:59:08 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -1,14 +1,47 @@
 package com.example.sunnyweather.logic
 
 import androidx.lifecycle.liveData
+import com.example.sunnyweather.logic.dao.PlaceDao
 import kotlinx.coroutines.Dispatchers
 import com.example.sunnyweather.logic.model.Place
+import com.example.sunnyweather.logic.model.Weather
 import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 //仓库层.仓库层的主要工作就是判断调用方请求的数据应该是从本地数据源中获取还是从网络数据源中获
 //取，并将获得的数据返回给调用方。因此，仓库层有点像是一个数据获取与缓存的中间层，在
 //本地没有缓存数据的情况下就去网络层获取，如果本地已经有缓存了，就直接将缓存数据返回。
 object Repository {
+    fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            coroutineScope {
+                val deferredRealtime = async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                    val weather = Weather(realtimeResponse.result.realtime,
+                        dailyResponse.result.daily)
+                    Result.success(weather)
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Result.failure<Weather>(e)
+        }
+        emit(result)
+    }
     fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
         val result = try {
             val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
@@ -23,4 +56,8 @@ object Repository {
         }
         emit(result)
     }
+    fun savePlace(place: Place) = PlaceDao.savePlace(place)
+    fun getSavedPlace() = PlaceDao.getSavedPlace()
+    fun isPlaceSaved() = PlaceDao.isPlaceSaved()
+
 }
